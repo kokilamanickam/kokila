26bc3c90cbd0dde2ef185d07463edaddd1a46ea5
kokilamanickam <30343812+kokilamanickam@users.noreply.github.com>
2017-08-05 16:51:59 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int num;
+cout<<"enter the num";
+cin>>num;
+if(num%2==0)
+{
+cout<<"the number is even";
+}
+else
+{
+cout<<"the number is odd";
+}
+getch();
+}
