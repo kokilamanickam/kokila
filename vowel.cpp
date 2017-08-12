5cc965804fae9ecb15b7c24250ef1f189bea7e57
kokilamanickam <30343812+kokilamanickam@users.noreply.github.com>
2017-08-12 16:52:16 +0530

    Create vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,31 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+char ch;
+cout<<"enter the character";
+cin>>ch;
+switch(ch)
+{
+case A:
+  cout<<"vowel";
+  break;
+case E:
+  cout<<"vowel";
+  break;
+case I:
+  cout<<"vowel";
+  break;
+case O:
+  cout<<"vowel";
+  break;
+case U:
+  cout<<"vowel";
+  break;
+default:
+  cout<< "consonant";
+  }
+  getch();
+  }
+  
